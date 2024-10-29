cda5fffe1c31e60ef44a34b7d25949f01edc5b3e
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2024-10-30 00:05:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort on Linked List/quick-sort-on-linked-list.cpp
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/* a node of the singly linked list */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* A utility function to insert a node at the beginning of linked list */
+void push(struct Node*& head_ref, int new_data) {
+    /* allocate node */
+    struct Node* new_node = new Node(new_data);
+
+    /* link the old list off the new node */
+    new_node->next = head_ref;
+
+    /* move the head to point to the new node */
+    head_ref = new_node;
+}
+
+/* A utility function to print linked list */
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+
+/* a node of the singly linked list
+struct node
+{
+    int data;
+    struct node *next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}; */
+
+// Solution class with quickSort function
+class Solution {
+    Node* splitList(Node* head) {
+        Node* slow = head;
+        Node* fast = head->next;
+    
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+    
+        Node* secondHalf = slow->next;
+        slow->next = nullptr;
+        return secondHalf;
+    }
+    
+    // Function to merge two sorted linked lists
+    Node* merge(Node* first, Node* second) {
+        if (!first) return second;
+        if (!second) return first;
+    
+        if (first->data < second->data) {
+            first->next = merge(first->next, second);
+            return first;
+        } else {
+            second->next = merge(first, second->next);
+            return second;
+        }
+    }
+    
+    // Merge Sort function for linked list
+    Node* mergeSort(Node* head) {
+        if (!head || !head->next)
+            return head;
+    
+        Node* secondHalf = splitList(head);
+        head = mergeSort(head);
+        secondHalf = mergeSort(secondHalf);
+    
+        return merge(head, secondHalf);
+    }
+  public:
+    struct Node* quickSort(struct Node* head) {
+        head = mergeSort(head);
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        head = ob.quickSort(head);
+
+        printList(head);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
